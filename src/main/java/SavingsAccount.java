bd89669f37d0ba326c4703e4e24529c2e4d1499d
Bartolem <97914472+Bartolem@users.noreply.github.com>
2023-11-22 15:52:39 +0100

    add SavingsAccount class

---
+++ src/main/java/SavingsAccount.java
@@ -0,0 +1,40 @@
+public class SavingsAccount extends Account {
+    private final double INTEREST_RATE;
+    private int minBalance;
+
+    public SavingsAccount(String ownerName, int minBalance) {
+        super(ownerName);
+        this.INTEREST_RATE = 0.3;
+        this.minBalance = minBalance;
+    }
+
+    public SavingsAccount( String ownerName, double balance, int minBalance) {
+        super(ownerName, balance);
+        this.INTEREST_RATE = 0.3;
+        this.minBalance = minBalance;
+    }
+
+    public void calculateInterestRate() {
+        setBalance(getBalance() + (getBalance() * INTEREST_RATE / 100));
+    }
+
+    public void setMinBalance(int amount) {
+        if (amount > 0) {
+            this.minBalance = amount;
+        }
+    }
+
+    @Override
+    public boolean withdraw(double amount) {
+        if ((getBalance() - amount >= minBalance)) {
+            setBalance(getBalance() - amount);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "(Savings Account) \n" + super.toString();
+    }
+}
