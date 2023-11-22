a8b243dcb981663b0c281627956acf1ccd892de1
Bartolem <97914472+Bartolem@users.noreply.github.com>
2023-11-22 13:25:36 +0100

    add generateRandomAccountNumber method to Account class

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,29 +1,38 @@
+import java.util.Random;
+
 public class Account {
     private int accountNumber;
     private double balance;
     private String ownerName;
+    private Random random;
 
-    public Account(int accountNumber, String ownerName) {
-        this.accountNumber = accountNumber;
+    public Account(String ownerName) {
+        this.random = new Random();
+        this.accountNumber = generateRandomAccountNumber();
         this.balance = 0;
         this.ownerName = ownerName;
     }
 
-    public Account(int accountNumber, double balance, String ownerName) {
-        this.accountNumber = accountNumber;
+    public Account(double balance, String ownerName) {
+        this.random = new Random();
+        this.accountNumber = generateRandomAccountNumber();
         this.balance = balance;
         this.ownerName = ownerName;
     }
 
+    public void setBalance(double amount) {
+        this.balance = amount;
+    }
+
     public void deposit(double amount) {
         if (amount > 0) {
-            this.balance += amount;
+            setBalance(amount);
         }
     }
 
     public boolean withdraw(double amount) {
-        if (amount > 0 && (balance - amount) > 0) {
-            this.balance -= amount;
+        if (amount > 0 && (balance - amount) >= 0) {
+            setBalance(amount);
             return true;
         }
         return false;
@@ -31,6 +40,10 @@ public class Account {
 
     public double getBalance() {
         return balance;
+    }
+
+    private int generateRandomAccountNumber() {
+        return random.nextInt(1_000_000, 10_000_000);
     }
 
     @Override
