113b0244be9a2b99400a6c3f45db2e210f04ec2c
Bartolem <97914472+Bartolem@users.noreply.github.com>
2023-11-28 17:44:58 +0100

    add tests for Bank class

---
+++ src/test/java/BankTest.java
@@ -0,0 +1,64 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankTest {
+    Bank bank;
+    Account account = new Account("Adam", 2000);
+
+    void addTwoAccounts() {
+        bank.add(new SavingsAccount("Jarek", 1000, 1000));
+        bank.add(new CurrentAccount("Bartolem", 300));
+    }
+
+    @BeforeEach
+    void createBankObject() {
+        bank = new Bank();
+        bank.add(account);
+    }
+    @Test
+    void addOneAccount() {
+        assertFalse(bank.isEmpty());
+        assertEquals(1, bank.size());
+    }
+
+    @Test
+    void addAFewDifferentAccount() {
+        addTwoAccounts();
+        assertFalse(bank.isEmpty());
+        assertEquals(3, bank.size());
+    }
+
+    @Test
+    void getAccount() {
+        assertNotNull(bank.getAccount(account.getACCOUNT_NUMBER()));
+    }
+
+    @Test
+    void getAllAccountsIfThereIsNoAccount() {
+        bank.remove(account.getACCOUNT_NUMBER());
+        assertTrue(bank.isEmpty());
+    }
+
+    @Test
+    void getAllAccountsIfThereIsAtLeastOne() {
+        assertFalse(bank.isEmpty());
+        assertNotNull(bank.getAllAccounts());
+        assertEquals(1, bank.size());
+    }
+
+    @Test
+    void remove() {
+        addTwoAccounts();
+        bank.remove(account.getACCOUNT_NUMBER());
+        assertFalse(bank.isEmpty());
+        assertEquals(2, bank.size());
+    }
+
+    @Test
+    void removeIfThereIsOnlyOneAccount() {
+        bank.remove(account.getACCOUNT_NUMBER());
+        assertTrue(bank.isEmpty());
+    }
+}
