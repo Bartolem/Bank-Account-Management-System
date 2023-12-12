011ab9baa5a47aef2d8c3fe5122fbbc070a3cafe
Bartolem <97914472+Bartolem@users.noreply.github.com>
2023-12-12 12:50:34 +0100

    add Admin class

---
+++ src/main/java/Admin.java
@@ -0,0 +1,16 @@
+public class Admin extends User {
+    private static Admin admin;
+
+    private Admin(Person person, Role role) {
+        super(person, role);
+    }
+
+    public static Admin getInstance() {
+        if (admin == null) {
+            Address address = new Address("Kasztanowa 16", "Lodz", "Poland", "28-990");
+            admin = new Admin(new Person("c2d0efc8-5db9-448f-9de2-04551551c769","Bartosz", "Adminek", "1999-05-14", address, "bronko@mail.com", "926 616 547"), Role.ADMIN);
+            Authentication.getInstance().addUserCredential("admin", admin);
+        }
+        return admin;
+    }
+}
