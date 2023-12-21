c6e36449fce8d6bd5c3f55ca424a885eb4e3e576
Bartolem <97914472+Bartolem@users.noreply.github.com>
2023-12-21 13:58:39 +0100

    implement functionality to corerect person details in UserInterface class

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -27,8 +27,7 @@ public class UserInterface {
                 case "2":
                     // Create new account
                     User user = createUser();
-                    Account account = createAccount(user);
-                    addUserAndAccountToBank(user, account);
+                    addUserAndAccountToBank(user, createAccount(user));
                     break;
                 case "X":
                 case "x":
@@ -150,11 +149,7 @@ public class UserInterface {
         return null;
     }
 
-
-
     private String validatePersonDetails(PersonDetail detail) {
-        String input = "";
-
         while (true) {
             if (detail.equals(PersonDetail.DATE_OF_BIRTH)) {
                 System.out.println("Valid date format: yyyy-MM-dd");
@@ -162,7 +157,7 @@ public class UserInterface {
 
             System.out.print(detail + ": ");
 
-            input = scanner.nextLine();
+            String input = scanner.nextLine();
 
             if (input.isEmpty()) {
                 System.out.println("Enter the " + detail);
@@ -179,7 +174,7 @@ public class UserInterface {
                         else System.out.println("Invalid " + detail);
                     }
                     case PHONE_NUMBER -> {
-                        if (validation.validatePhoneNumber(input)) return input;
+                        if (validation.validatePhoneNumber(input)) return input.replaceAll("[^0-9]", "");
                         else System.out.println("Invalid " + detail);
                     }
                     default -> {
@@ -191,9 +186,6 @@ public class UserInterface {
     }
 
     private User createUser() {
-        Address address;
-        Person person;
-        User user;
         ArrayList<String> personDetails = new ArrayList<>();
 
         printCreateUserMessage();
@@ -202,29 +194,38 @@ public class UserInterface {
             personDetails.add(validatePersonDetails(personDetail));
         }
 
-        address = new Address(personDetails.get(3), personDetails.get(4), personDetails.get(5), personDetails.get(6));
-        person = new Person(personDetails.get(0), personDetails.get(1), personDetails.get(2), address, personDetails.get(7), personDetails.get(8));
+        return confirmDetails(personDetails);
+    }
 
+    private User confirmDetails(ArrayList<String> personDetails) {
         System.out.println("Are you confirm the provided details? (y/n)");
         printCursor();
         String answer = scanner.nextLine();
 
         if (answer.equalsIgnoreCase("yes") || answer.equalsIgnoreCase("y")) {
-            user = new User(person);
-            return user;
+            Address address = new Address(personDetails.get(3), personDetails.get(4), personDetails.get(5), personDetails.get(6));
+            return new User(new Person(personDetails.get(0), personDetails.get(1), personDetails.get(2), address, personDetails.get(7), personDetails.get(8)));
         } else if (answer.equalsIgnoreCase("no") || answer.equalsIgnoreCase("n")) {
-            System.out.println("(1) Quit\n(2) Start again\n(3) Correct some details");
+            System.out.println("(1) Correct some details\n(2) Start again\n(X) Quit");
+            printCursor();
             String secondAnswer = scanner.nextLine();
 
             switch (secondAnswer) {
                 case "1":
+                    // Implement details correcting logic
+                    PersonDetail correctedDetail = correctPersonDetail();
+                    int index = correctedDetail.ordinal();
+                    String validatedAndCorrectedDetail = validatePersonDetails(correctedDetail);
+
+                    System.out.println("Old value: \"" + personDetails.get(index) + "\" -> " + "New value: \"" + validatedAndCorrectedDetail + "\"");
+                    personDetails.set(index, validatedAndCorrectedDetail);
+                    confirmDetails(personDetails);
                     break;
                 case "2":
                     createUser();
                     break;
-                case "3":
-                    System.out.print("Which details you want to correct?: ");
-                    // Implement details correcting logic
+                case "X":
+                case "x":
                     break;
             }
         }
@@ -232,9 +233,51 @@ public class UserInterface {
         return null;
     }
 
+    private PersonDetail correctPersonDetail() {
+        System.out.println("Which details you want to correct?");
+        for (PersonDetail detail : PersonDetail.values()) {
+            System.out.println("(" + (detail.ordinal() + 1) + ") " + detail);
+        }
+
+        while (true) {
+            printCursor();
+            String answer = scanner.nextLine();
+
+            switch (answer) {
+                case "1" -> {
+                    return PersonDetail.FIRST_NAME;
+                }
+                case "2" -> {
+                    return PersonDetail.LAST_NAME;
+                }
+                case "3" -> {
+                    return PersonDetail.DATE_OF_BIRTH;
+                }
+                case "4" -> {
+                    return PersonDetail.STREET_ADDRESS;
+                }
+                case "5" -> {
+                    return PersonDetail.CITY;
+                }
+                case "6" -> {
+                    return PersonDetail.COUNTRY;
+                }
+                case "7" -> {
+                    return PersonDetail.ZIP_CODE;
+                }
+                case "8" -> {
+                    return PersonDetail.EMAIL;
+                }
+                case "9" -> {
+                    return PersonDetail.PHONE_NUMBER;
+                }
+            }
+        }
+    }
+
     private void printCreateUserMessage() {
         System.out.println("To create new account, you need to provide your details.");
-        System.out.println("Such as first name, last name, date of birth, address, e-mail and phone number");
+        System.out.println(Arrays.toString(PersonDetail.values()));
     }
 
     private void printStartingMessage() {
