e53fcc602ca7b1451847c5e23586b89968a26b3e
Bartolem <97914472+Bartolem@users.noreply.github.com>
2023-12-21 11:52:57 +0100

    move validation methods from UserInterface to newly created Validation class

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,14 +1,14 @@
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class UserInterface {
     private final Bank bank;
     private final Scanner scanner;
+    private final Validation validation;
 
-    public UserInterface(Scanner scanner) {
+    public UserInterface(Scanner scanner, Validation validation) {
         this.bank = Bank.getInstance();
         this.scanner = scanner;
+        this.validation = validation;
     }
 
     public void start() {
@@ -150,25 +150,7 @@ public class UserInterface {
         return null;
     }
 
-    public boolean validateDateOfBirth(String Date) {
-        Pattern pattern = Pattern.compile("^((19|2[0-9])[0-9]{2})-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$");
-        Matcher matcher = pattern.matcher(Date);
-        return matcher.matches();
-    }
 
-    public boolean validatePhoneNumber(String phoneNumber) {
-        // Allow 9-10 digits and optional whitespace, dots, or hyphens (-) between the numbers
-        Pattern pattern = Pattern.compile("^(\\d{3}[- .]?){2}\\d{3,4}$");
-        Matcher matcher = pattern.matcher(phoneNumber);
-        return matcher.matches();
-    }
-
-    public boolean validateEmail(String email) {
-        // Regular expression for email validation provided by the RFC 5322 standards
-        Pattern pattern = Pattern.compile("^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$");
-        Matcher matcher = pattern.matcher(email);
-        return matcher.matches();
-    }
 
     private String validatePersonDetails(PersonDetail detail) {
         String input = "";
@@ -187,17 +169,17 @@ public class UserInterface {
             } else {
                 switch (detail) {
                     case DATE_OF_BIRTH -> {
-                        if (validateDateOfBirth(input)) return input;
+                        if (validation.validateDateOfBirth(input)) return input;
                         else {
                             System.out.println("Invalid " + detail);
                         }
                     }
                     case EMAIL -> {
-                        if (validateEmail(input)) return input;
+                        if (validation.validateEmail(input)) return input;
                         else System.out.println("Invalid " + detail);
                     }
                     case PHONE_NUMBER -> {
-                        if (validatePhoneNumber(input)) return input;
+                        if (validation.validatePhoneNumber(input)) return input;
                         else System.out.println("Invalid " + detail);
                     }
                     default -> {
---
+++ src/main/java/Validation.java
@@ -0,0 +1,24 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validation {
+    public boolean validateDateOfBirth(String Date) {
+        Pattern pattern = Pattern.compile("^((19|2[0-9])[0-9]{2})-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$");
+        Matcher matcher = pattern.matcher(Date);
+        return matcher.matches();
+    }
+
+    public boolean validatePhoneNumber(String phoneNumber) {
+        // Allow 9-10 digits and optional whitespace, dots, or hyphens (-) between the numbers
+        Pattern pattern = Pattern.compile("^(\\d{3}[- .]?){2}\\d{3,4}$");
+        Matcher matcher = pattern.matcher(phoneNumber);
+        return matcher.matches();
+    }
+
+    public boolean validateEmail(String email) {
+        // Regular expression for email validation provided by the RFC 5322 standards
+        Pattern pattern = Pattern.compile("^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$");
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
